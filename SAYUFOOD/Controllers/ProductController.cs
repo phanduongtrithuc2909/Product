7906bd78530c51a25e740a4a1dc23483e4888253
Trí Thức <kawaiiqw.123@gmail.com>
2023-05-18 18:41:10 +0700

    hien thu san pham

---
+++ SAYUFOOD/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using SAYUFOOD.Models;
+using SAYUFOOD.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAYUFOOD.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _dbcontext = new ApplicationDbContext();        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> items = _dbcontext.Products.ToList<Product>();
+            return View(items);
+        }
+    }
+}
